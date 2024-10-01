28796176e042c4c2e42f4a74853a8a754e5eac59
PixelSanctuary <sanctuarypixel@gmail.com>
2024-10-01 16:56:44 +0530

    're > are
    're throws build err replaced with are

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React from 'react';
+import React from  'react';
 import Image from "next/image";
 import graphLines from '@/app/assets/graphLines.png';
 
@@ -15,8 +15,8 @@ const About = () => {
       <div className="flex flex-col text-center gap-5 about-content max-w-4xl mx-auto px-5 sm:px-8 lg:px-16 py-10 sm:text-lg">
         <h1 className="text-4xl font-semibold">About Pixel Sanctuary</h1>
         <p className='text-white/80'>
-          Welcome to <strong>Pixel Sanctuary</strong>! We’re a passionate team of developers, gamers, and creators
-          dedicated to shaping the future of gaming through cutting-edge software development. Whether you're a seasoned 
+          Welcome to <strong>Pixel Sanctuary</strong>! We are a passionate team of developers, gamers, and creators
+          dedicated to shaping the future of gaming through cutting-edge software development. Whether you are a seasoned 
           developer or a passionate gamer, Pixel Sanctuary is the place for you to connect, create, and innovate.
         </p>
 
@@ -29,8 +29,8 @@ const About = () => {
 
         <h2 className="text-3xl sm:text-4xl font-semibold  mt-10">Join the Movement</h2>
         <p className='text-white/80'>
-          We’re not just a team; we’re a movement. We invite you to become one of the <strong>Pixelers</strong> and help us 
-          shape the future of gaming. Whether you’re a developer, gamer, or a visionary, Pixel Sanctuary is your home.
+          We are not just a team; we are a movement. We invite you to become one of the <strong>Pixelers</strong> and help us 
+          shape the future of gaming. Whether you are a developer, gamer, or a visionary, Pixel Sanctuary is your home.
         </p>
 
         <h2 className="text-3xl sm:text-4xl font-semibold  mt-10">Why Choose Pixel Sanctuary?</h2>
